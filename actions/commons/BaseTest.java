e9dacb160b993aab262e7f4b1e61a89bcbae9f83
Sang Le <sangle.qc.98@gmail.com>
2022-03-24 11:58:47 +0700

    Framework 09 - Headless/ CocCoc/ Opera/ Brave browser/ WebDriverManager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,28 +4,44 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	private WebDriver driverBaseTest;
-	private String projectPath = System.getProperty("user.dir");
+	private WebDriver driver;
 
-	public WebDriver GetBrowserDriver(String browserName) {
+	protected WebDriver GetBrowserDriver(String browserName) {
 		if (browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driverBaseTest = new FirefoxDriver();
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
 		} else if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-			driverBaseTest = new ChromeDriver();
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
 		} else if (browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driverBaseTest = new EdgeDriver();
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equals("coccoc")) {
+			WebDriverManager.chromedriver().driverVersion("98.0.4758.102").setup();
+			ChromeOptions option = new ChromeOptions();
+			option.setBinary("C:\\Users\\Admin\\AppData\\Local\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(option);
+		} else if (browserName.equals("brave")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions option = new ChromeOptions();
+			option.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+			driver = new ChromeDriver(option);
+		} else if (browserName.equals("opera")) {
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
 		} else {
 			throw new RuntimeException("Browser name is incorrect !");
 		}
-		driverBaseTest.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driverBaseTest.get("https://demo.nopcommerce.com/");
-		return driverBaseTest;
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		return driver;
 	}
 }
